901e987db8ad65cf4c265c40cb64b75ff7d325de
Gil12401 <cigr45@naver.com>
2025-08-18 08:49:55 +0900

    Title: 2021년 날짜의 유무, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250818/2021년 날짜의 유무/with-or-without-2021.py
+++ 250818/2021년 날짜의 유무/with-or-without-2021.py
@@ -4,7 +4,7 @@ M, D = map(int, input().split())
 
 def chk_valid_day_in_2021(M,D):
     last_day_in_2021 = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    valid_date = (1 <= M <= 12) and (1 <= D <= last[M-1])
+    valid_date = (1 <= M <= 12) and (1 <= D <= last_day_in_2021[M-1])
     if (valid_date):
         return "Yes"
     else:
