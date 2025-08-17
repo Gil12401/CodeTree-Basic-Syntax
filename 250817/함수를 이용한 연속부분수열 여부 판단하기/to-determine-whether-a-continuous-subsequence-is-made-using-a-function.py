bbfe96a8ff164bbd8b91a7e0d0cb3c7ddc820efa
Gil12401 <cigr45@naver.com>
2025-08-17 17:15:52 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250817/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 250817/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -2,20 +2,20 @@ n1, n2 = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
             
-# slice 
-def chk_subsequnce(a, b):
-    if (b[0] in a):
-        begin = a.index(b[0])
-        end = begin + len(b)
-        tmp_subseq = a[begin:end]
-    
-        if(b == tmp_subseq):
-            return "Yes"
-        else:
-            return "No"
-    else:
-        return "No"
 
+def chk_subsequnce(a, b):
+    result = "No"
+    for i in range(len(a)):
+        if(a[i] == b[0]):
+            begin = i
+            end = min(len(a),begin + len(b))
+            tmp_subseq = a[begin:end]
+            if(tmp_subseq == b):
+                result = "Yes"
+            
+    return result    
+          
+      
 
 print(chk_subsequnce(a,b))
      
