3d1961314bef1daebd1945e47e4ee0f79226ba26
Gil12401 <cigr45@naver.com>
2025-08-19 21:24:03 +0900

    Title: k번째로 신기한 문자열, Time: 54ms, Memory: 16MB, Status: Passed - Codetree

--- 250819/k번째로 신기한 문자열/kth-special-string.py
+++ 250819/k번째로 신기한 문자열/kth-special-string.py
@@ -3,30 +3,25 @@ n, k = int(n), int(k)
 str = [input() for _ in range(n)]
 
 # Please write your code here.
-def chk_substr(string, pattern):
-    result = False
-    compare_count = len(string) - len(pattern) + 1 
-    for i in range(compare_count):
-        _substr = string[i:i+len(pattern)]
-        if(_substr == pattern):
-            result = True
-    
-    return result
+def chk_start_with_T(string, pattern): 
+    if len(string) < len(pattern):
+        return False
+
+    for i in range(len(pattern)):
+        if(string[i] != pattern[i]):
+            return False
+
+    return True
 
 def get_sorted_substr_list(str):
     sorted_substr = []
     sorted_str = sorted(str)
 
     for word in sorted_str:
-        if(chk_substr(word, t)):
+        if(chk_start_with_T(word, t)):
             sorted_substr.append(word)
     
     return sorted_substr
 
 _sorted_substr = get_sorted_substr_list(str)
 print(_sorted_substr[k-1])
-
-
-        
-
-    
