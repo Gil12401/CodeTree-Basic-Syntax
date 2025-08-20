19ef1f36adf43161e049a59025bfaed4ff4cf648
Gil12401 <cigr45@naver.com>
2025-08-20 16:36:47 +0900

    Title: Date to Date, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250820/Date to Date/date-to-date.py
+++ 250820/Date to Date/date-to-date.py
@@ -6,14 +6,14 @@ last_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 def get_days_from_date(m,d):
     days = 0
     days += d 
-    for i in range(m):
+    for i in range(m-1):
         days += last_days[i]
 
     return days
 
 days_start = get_days_from_date(m1,d1)
 days_end = get_days_from_date(m2,d2)
-days_diff = days_end - days_start
+days_diff = days_end - days_start + 1 
 
 print(days_diff)
 
