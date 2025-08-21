35881d658ccbef96eeddf7887b7dfec4138f474e
Gil12401 <cigr45@naver.com>
2025-08-21 10:11:05 +0900

    Title: 2진수로 변환하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250821/2진수로 변환하기/convert-to-binary.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+# Please write your code here.
+def decimal_to_binary(n):
+    digits= []
+
+
+    while True:
+        if n < 2:
+            digits.append(n)
+            break
+        digits.append(n%2)
+        n //= 2
+    
+    return digits[::-1]
+
+binary_digits = decimal_to_binary(n)
+for digit in binary_digits:
+    print(digit,end="")
