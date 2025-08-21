e27173cc35354bf6d1a306bef3cd150892a29dc4
Gil12401 <cigr45@naver.com>
2025-08-21 09:25:25 +0900

    Title: 요일 맞추기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250821/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,33 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+# Please write your code here.
+last_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+# m1, d1 -> monday 
+def get_days_from_date(m,d):
+    days = 0
+    days += d 
+    for i in range(m-1):
+        days += last_days[i]
+
+    return days
+
+
+def get_day_of_week(mon_days,unknown_days):
+    day_of_week = []
+    
+    if(mon_days <= unknown_days):
+        day_of_week =["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    else:
+        day_of_week = ["Mon", "Sun", "Sat", "Fri", "Thu", "Wed", "Tue"]
+        
+    day_idx = abs((mon_days - unknown_days) % 7)
+    
+    return day_of_week[day_idx]
+
+
+mon_days = get_days_from_date(m1,d1)
+unknown_days = get_days_from_date(m2,d2)
+
+
+print(get_day_of_week(mon_days,unknown_days))
