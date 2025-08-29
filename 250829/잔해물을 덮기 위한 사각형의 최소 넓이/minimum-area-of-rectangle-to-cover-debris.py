13cbab1d51fff9ab49ef8540d9a728f8dd9f6160
Gil12401 <cigr45@naver.com>
2025-08-29 14:27:32 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 136ms, Memory: 49MB, Status: Wrong Answer - Codetree

--- 250829/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 250829/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -18,23 +18,21 @@ for i in range(2):
 Ax1, Ax2 = sorted((x1[0] + offset, x2[0] + offset))
 Ay1, Ay2 = sorted((y1[0] + offset, y2[0] + offset))
 
-x_max, y_max = 0, 0
-for row in range(Ay1, Ay2):  
-    if any(cell == 1 for cell in grid[row]):
-        y_max = row
-
+width_max = 0
+height = Ay2 - Ay1
+for row in range(Ay1, Ay2):
+    width_count = 0 
     for col in range(Ax1, Ax2):
-        if(grid[row][col] == 1 and x_max <= col):
-            x_max = col 
+        if(grid[row][col] == 1):
+            width_count += 1
+        else:
+            width_count = 0 
 
-# print("x_max",x_max,"y_max",y_max)
-# 'A만 칠해진' 칸만 카운트 (겹친 곳은 2가 되므로 제외)
-area = 0
-for row in range(Ay1, y_max + 1):      # y축 → 행
-    for col in range(Ax1, x_max + 1):  # x축 → 열
-        # print(grid[row][col], end=" ")
-        if(grid[row][col] >= 1):
-            area += 1
-    # print()
+        if all(x == 2 for x in grid[row]):
+            height -= 1
+    
+    width_max = max(width_max,width_count)
 
+
+area = width_max * height
 print(area)
