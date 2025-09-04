60083f1f3e9e52032f0b74de56b52bff1e83f902
Gil12401 <cigr45@naver.com>
2025-09-04 09:57:24 +0900

    Title: 연속되는 수 2, Time: 61ms, Memory: 16MB, Status: Passed - Codetree

--- 250904/연속되는 수 2/continuous-number2.py
+++ 250904/연속되는 수 2/continuous-number2.py
@@ -7,8 +7,7 @@ count = 0
 max_count = 0
 for i in range(n):
     if (i == 0 or arr[i] != arr[i-1]):
-        max_count = max(max_count, count)
         count = 0
     count += 1
-
+    max_count = max(max_count, count)
 print(max_count)
